e314faa67e0a74498beb96d8e7b8b21c03902b1f
kvarela <karim@metalink.com>
2023-06-26 19:46:52 -0700

    Added JwtAuthGuard for currentUser()

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,10 +1,13 @@
 import { Resolver, Query } from '@nestjs/graphql'
 import { User } from './entities/user.entity'
 import { CurrentUser } from '../auth/current-user.decorator'
+import { UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 
 @Resolver()
 export class UsersResolver {
   @Query(() => User)
+  @UseGuards(JwtAuthGuard)
   async currentUser(@CurrentUser() user: User) {
     return user
   }
