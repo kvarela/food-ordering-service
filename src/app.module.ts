eebeab5687c5a75678eb213d72b1dde409dd994c
kvarela <karim@metalink.com>
2023-06-26 18:35:05 -0700

    Cleaned up root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './database/database.config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { RestaurantModule } from './restaurant/restaurant.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { config } from './database/database.config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
+import { RestaurantModule } from './restaurant/restaurant.module'
 
 @Module({
   imports: [
@@ -16,13 +15,12 @@ import { RestaurantModule } from './restaurant/restaurant.module';
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: true,
-      sortSchema: true,
+      sortSchema: true
     }),
     UsersModule,
     AuthModule,
-    RestaurantModule,
+    RestaurantModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController]
 })
 export class AppModule {}
